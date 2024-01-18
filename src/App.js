2f9a2efe7f3aa754f6fda652bcfc7e241804b8d8
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-01-18 13:37:34 +0530

    understanding some basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <h1 title="MY APP">A React App!</h1>;  //same as React.createElement('h1',{title:'MY APP'},'Hi this is reactJS');
 };
+
+// class App extends React.Component{
+//   render(){
+//     return <h1 title="MY APP">A React App Part 2!</h1>; 
+//   }
+// }   this is the same as the above jsx function
 
 export default App;
