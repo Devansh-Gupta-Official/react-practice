92fff8bf9d2a75e2bfb40c2daffad0277306c8f2
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-01-19 16:25:28 +0530

    using props to pass data between components

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import './GoalList.css'
 
-const GoalList = () =>{
+const GoalList = props =>{
+    console.log(props.goals);
     return (
         <ul className="goal-list">
-        <li>Finish the Course.</li>
-        <li>Learn all about the Course.</li>
-        <li>Help other students in the Course.</li>
+        
       </ul>
     );
 };
