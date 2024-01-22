4b5645d3b61460ff5ba31a6a1acdafb8be75ae4d
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-01-22 15:28:52 +0530

    two way binding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+
 import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal'
+import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
-  const [courseGoals,setCourseGoals] = useState([{id:'cg1',text:'Finish the Course'},{id:'cg2',text:'Learn all about the Course.'},{id:'cg3',text:'Help other students in the Course.'}
-]); 
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ]);
 
-  const addNewGoalHandler = (newGoal) => {
+  const addNewGoalHandler = newGoal => {
     // setCourseGoals(courseGoals.concat(newGoal));
-    setCourseGoals((prevCourseGoals) => {
-      return [prevCourseGoals.concat(newGoal)]
-    });
-
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
   };
 
-  return( 
-    <div className='course-goals'>
+  return (
+    <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHandler}/>
-      <GoalList goals={courseGoals}/>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import './NewGoal.css'
+import React, { useState } from 'react';
+
+import './NewGoal.css';
 
 const NewGoal = props => {
-    const addGoalHandler = event => {
-        event.preventDefault();
-        const newGoal = {
-            id:Math.random().toString(),
-            text:"My new goal!"
-        };
+  const [enteredText, setEnteredText] = useState('');
 
-        props.onAddGoal(newGoal);
+  const addGoalHandler = event => {
+    event.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: enteredText
     };
 
-    return <form className='new-goal' onSubmit={addGoalHandler}>
-        <input type='text'/>
-        <button type="submit">Add Goal</button>
+    setEnteredText('');
+
+    props.onAddGoal(newGoal);
+  };
+
+  const textChangeHandler = event => {
+    setEnteredText(event.target.value);
+  };
+
+  return (
+    <form className="new-goal" onSubmit={addGoalHandler}>
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
+      <button type="submit">Add Goal</button>
     </form>
+  );
 };
 
 export default NewGoal;
